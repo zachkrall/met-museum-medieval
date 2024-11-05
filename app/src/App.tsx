7463829f0525494736c015a7b5f4dcaecce3ad48
Zach Krall <zach@zachkrall.com>
2024-11-04 19:27:45 -0500

    update api call and add better loading state

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,26 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Plot } from "./components/Plot";
 import { useGetObjects } from "./api/useGetObjects";
+import { LoaderIcon } from "lucide-react";
 
 const queryClient = new QueryClient();
 
-function Main () {
+function Main() {
   const objects = useGetObjects();
 
-  if(objects.isLoading){
-    return <div>Loading...</div>
+  if (objects.isLoading) {
+    return (
+      <div
+        className={
+          "fixed inset-0 bg-black text-white grid place-content-center"
+        }
+      >
+        <div className={"flex flex-col gap-4"}>
+          <LoaderIcon className={"size-4 animate-spin opacity-50"} />
+          <div>Loading...</div>
+        </div>
+      </div>
+    );
   }
 
   return <Plot objects={objects} />;
@@ -17,7 +29,7 @@ function Main () {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Main/>
+      <Main />
     </QueryClientProvider>
   );
 }
--- app/src/api/useGetObjects.ts
+++ app/src/api/useGetObjects.ts
@@ -4,7 +4,7 @@ export const useGetObjects = () => {
   return useQuery({
     queryKey: ["objectsEmbedding"],
     queryFn: async function () {
-      const response = await fetch("/embedding.json");
+      const response = await fetch("./embedding.json");
       const json = await response.json();
 
       return json as { objectID: string; x: number; y: number }[];
